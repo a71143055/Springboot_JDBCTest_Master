952e6b1dc1b72126d49e19d661b5ff75eef5e36f
a71143055 <a71143055@gmail.com>
2025-09-22 10:05:03 +0900

    JDBC

--- src/main/java/kr/ac/kopo/JGY/springboot_jdbctest/repository/DetailRepository.java
+++ src/main/java/kr/ac/kopo/JGY/springboot_jdbctest/repository/DetailRepository.java
@@ -1,10 +1,10 @@
 package kr.ac.kopo.JGY.springboot_jdbctest.repository;
 
-import kr.ac.kopo.JGY.springboot_jdbctest.domain.Product;
+import kr.ac.kopo.JGY.springboot_jdbctest.domain.Detail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetailRepository extends JpaRepository<Product,Integer> {
+public interface DetailRepository extends JpaRepository<Detail,Integer> {
 
 }
